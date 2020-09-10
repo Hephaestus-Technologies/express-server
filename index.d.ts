c147b88bb6de5d8b5680003f1f54a21f93d56d5b
Nathan Floris Copier <nfcopier1@aggiemail.usu.edu>
2020-09-10 05:07:32 -0600

    Converts to typescript

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as core from "express-serve-static-core";
-
-interface ExpressServer extends core.Application {
-
-    start(): void;
-
-}
-
-export default function ({port, assets, clientName} : {port: number, assets: string[], clientName: string}) : ExpressServer;
